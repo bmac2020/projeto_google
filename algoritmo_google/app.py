a666b650bf350de9fc7e8f7c9a7123938eb42360
v3ntur4X <51869757+v3ntur4X@users.noreply.github.com>
2020-08-06 18:58:56 -0300

    Atualização app

--- algoritmo_google/app.py
+++ algoritmo_google/app.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 from models import GeraGrafo
+from core import GeraMatriz
+from views import MostraMatriz
 
 def main():
     k = input("Digite o k: ")
     grafo = GeraGrafo(k)
     grafo.listaNodeCacique()
     grafo.criaGrafo()
-    print(grafo.edges())
+    n_nodes = grafo.n_nodes()
+    arestas = grafo.arestas()
+
+    gera = GeraMatriz(grafo, n_nodes, arestas)
+    matriz = gera.geraMatriz()
+
+    print(MostraMatriz(matriz).mostraMatriz())
 
 if __name__ == "__main__":
     main()
