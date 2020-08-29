b67a758f72dbb0a01f0558fc64503e638be75e70
v3ntur4X <51869757+v3ntur4X@users.noreply.github.com>
2020-08-29 14:50:24 -0300

    Atualização do setup com informações dos autores

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ setup(
     version="0.1.0",
     description="Projeto de Modelagem Matemática - PageRank"
     long_description=readme,
-    author="Gabriel, Guilherme, Milton, Richard"
-    author_email=""
+    author="Gabriel Milani, Guilherme Ventura, Milton Leal, Richard Sousa"
+    author_email="gabriel.milani@usp.br, guilhermeventura@usp.br, milton.leal@usp.br, richardsousa@usp.br"
     url="https://github.com/bmac2020/projeto_google"
     license=license,
     packages=find_packages(exclude="tests")
