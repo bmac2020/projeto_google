f2ad123c6022f3b9070260070a9791d2fe0b6c46
v3ntur4X <51869757+v3ntur4X@users.noreply.github.com>
2020-08-29 17:32:55 -0300

    Comentários finais das classes do core

--- algoritmo_google/core/rede.py
+++ algoritmo_google/core/rede.py
@@ -1,32 +1,50 @@
 #!/usr/bin/python3
 
 class Rede:
-    def __init__(self):
-        self.nodes = []
-        self.conexao = []
+    """
+    Essa classe gera uma rede, onde guardará os nós e as conexões.
 
-    def adiciona_node(self, node):
+    Parâmetros:
+        - node : nó.
+        - nodes : lista de nós.
+        - conexao : uma conexão entre nós.
+        - conexoes : lista de conexões.
+
+    Funções:
+        - adiciona_node() : adiciona um nó a lista de nós da rede.
+        - adiciona_nodes() : adiciona os nós de uma lista a lista de nós da rede.
+        - adiciona_conexao() : adiciona uma conexao a lista de conexões da rede.
+        - adiciona_conexoes() : adiciona as conexões de uma lista a lista de conexões da rede.
+        - numero_total_nodes() : retorna o número total de nós da rede.
+        - conjunto_arestas() : retorna todas as conexões da rede.
+    """
+
+    def __init__(self):
+        self.nodes = [] # Lista de nós
+        self.conexao = [] # Lista de conexões
+
+    def adiciona_node(self, node): # Adiciona o nó na lista de nós.
         self.nodes.append(node)
 
-    def adiciona_nodes(self, nodes):
+    def adiciona_nodes(self, nodes): # Adiciona uma lista de nós.
         for node in nodes:
             if node not in self.nodes:
                 self.nodes.append(node)
 
-    def adiciona_conexao(self, conexao):
+    def adiciona_conexao(self, conexao): # Adiciona uma conexão.
         if conexao not in self.conexao:
-            if conexao[0] not in self.nodes:
+            if conexao[0] not in self.nodes: # Verifica se os nós dessa conexão não está na lista de nós.
                 self.nodes.append(conexao[0])
-            if conexao[1] not in self.nodes:
+            if conexao[1] not in self.nodes: # Verifica se os nós dessa conexão não está na lista de nós.
                 self.nodes.append(conexao[1])
             self.conexao.append(conexao)
 
-    def adiciona_conexoes(self, conexoes):
+    def adiciona_conexoes(self, conexoes): # Adiciona uma lista de conexões.
         for i in conexoes:
             self.adiciona_conexao(i)
 
-    def numero_total_nodes(self):
+    def numero_total_nodes(self): # Retorna o número de nós da rede.
         return len(self.nodes)
 
-    def conjunto_arestas(self):
+    def conjunto_arestas(self): # Retorna todas as conexões da rede.
         return self.conexao
